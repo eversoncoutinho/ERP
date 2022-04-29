b3643a18d4c35765ea337b69d2682a3a2e87c49c
eversoncoutinho <eversoncoutinho@gmail.com>
2022-04-29 12:48:36 +0000

    Instalação do BuildBundlerMinifier

--- ERP.WEB/Infra/Migrations/20220429093417_initialSQL3.Designer.cs
+++ ERP.WEB/Infra/Migrations/20220429093417_initialSQL3.Designer.cs
@@ -4,14 +4,16 @@ using Infra.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace ERP.WEB.Data.Migrations
+namespace Infra.Migrations
 {
     [DbContext(typeof(ERPDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220429093417_initialSQL3")]
+    partial class initialSQL3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
